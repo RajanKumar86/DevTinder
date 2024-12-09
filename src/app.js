d4dc52a2d11e68d9fa3303f3693eceeb0abe4a85
Rajan Kumar <rajankumar07.dev@gmail.com>
2024-12-10 04:16:06 +0530

    created a patch request.

--- src/app.js
+++ src/app.js
@@ -61,6 +61,18 @@ app.delete("/user", async (req, res) => {
   }
 });
 
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("user Updated suceesfully !!!");
+  } catch (err) {
+    res.status(400).send("something went wrong" + err.message);
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database is connected...");
