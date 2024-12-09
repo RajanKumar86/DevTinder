27368bd96f6b72b97625c9d5b823d11e1712805f
Rajan Kumar <rajankumar07.dev@gmail.com>
2024-12-09 15:05:53 +0530

    created 1st userData in DB

--- src/app.js
+++ src/app.js
@@ -4,6 +4,34 @@ const connectDB = require("./config/database");
 
 const app = express();
 const PORT = 5000;
+const User = require("./config/model/model");
+
+app.get("/", (req, res) => {
+  res.send("hello from the server 05 !!!");
+});
+app.get("/signup", (req, res) => {
+  res.send("this is Signup Page ");
+});
+
+app.post("/signup", async (req, res) => {
+  const userObj = {
+    firstName: "virat",
+    lastName: "kohli",
+    emailId: "viratkohli@gmail.com",
+    password: "iamvirat",
+    age: 32,
+    gender: "Male",
+  };
+
+  const user = new User(userObj);
+
+  try {
+    await user.save();
+    res.send("user added succesfully...");
+  } catch (err) {
+    res.status(400).send("something went wrong" + err.message);
+  }
+});
 
 connectDB()
   .then(() => {
